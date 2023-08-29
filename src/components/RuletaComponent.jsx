a7e9b89ca80ac8e0f542ae9c3e0cd35e01febb8f
estebanBaldillaTics <levintics@gmail.com>
2023-08-28 22:56:36 -0600

    Productive

---
+++ src/components/RuletaComponent.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const RuletaComponent = () =>{
+    return(
+        <div className="ruleta-container">
+            <h1>Ruleta</h1>
+        </div>
+    )
+};
+
+export default RuletaComponent;
